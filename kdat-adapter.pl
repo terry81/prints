8e2c5eb327c1947091f811c1b137d85f5a4b7a6e
Anatoliy Dimitrov <tollodim@gmail.com>
2014-10-22 14:55:12 +0300

    Small Kdat fix

--- kdat-adapter.pl
+++ kdat-adapter.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/perl
 # Kdat adapter
-#If used escape these characters . * ? + [ ] ( ) { } ^ $ | \ ;
-#The 's' RE modifier makes it work on more than one line by default.
 
 use strict;
 use warnings;
@@ -12,8 +10,7 @@ open INPUT, '<', $file or warn;
 my @input_array=<INPUT>;
 close(INPUT);
 my $file_code=join("",@input_array);
-#$file_code =~ s#KA#K1#g; #Be careful with the 's' RE modifier. 
-$file_code =~ s#(st.*\n)KA\;#$1K1;#g; #Be careful with the 's' RE modifier. 
+$file_code =~ s#(st.*\n)KA\;#$1K1;#g;
 open OUTPUT, '>', $file or warn;
 print(OUTPUT $file_code);
 close(OUTPUT);
