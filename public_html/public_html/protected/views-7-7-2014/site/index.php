82b7ce5163783270eb25a1e3ed48345600c201c0
tollodim@gmail.com <tollodim@gmail.com>
2015-05-16 23:26:06 +0300

    one more

--- public_html/public_html/protected/views-7-7-2014/site/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* @var $this SiteController */
-
-$this->pageTitle=Yii::app()->name;
-?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>This is the site for the next geheration of PRINTS. It's currently under development.</p>
